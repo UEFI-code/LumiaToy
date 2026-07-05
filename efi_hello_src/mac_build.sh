8993162d3259d5c5ebabf372e735fbb9b042f1b6
UEFI-code <zhuoshizhang@hotmail.com>
2026-07-05 09:23:39 +0900

    Add linker.ld, and link it to ELF instead

--- efi_hello_src/mac_build.sh
+++ efi_hello_src/mac_build.sh
@@ -6,10 +6,9 @@
   -c 1.c \
   -o 1.o
 
-/opt/homebrew/opt/lld/bin/lld-link \
-  /subsystem:efi_application \
-  /entry:efi_main \
-  /machine:arm \
-  /base:0x10000000 \
-  /align:4096 \
-  1.o /out:1.efi
+/opt/homebrew/opt/lld/bin/ld.lld \
+    -m armelf \
+    -T linker.ld \
+    -e efi_main \
+    1.o \
+    -o 1.elf
